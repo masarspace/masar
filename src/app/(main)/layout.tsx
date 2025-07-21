cbe040107c32404725c1dbb737e6790310be9819
Masar Space <MasarCoworkingspace@gmail.com>
2025-07-21 19:33:20 +0000

    we can separate the purchase order status page and purchase order tracki

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -162,7 +162,7 @@ export default function MainLayout({
           </SidebarContent>
         </Sidebar>
         <SidebarInset>
-          <main className="flex-1 flex flex-col">{children}</main>
+          <main className="flex-1 flex flex-col p-4 sm:p-6 lg:p-8">{children}</main>
         </SidebarInset>
       </div>
     </SidebarProvider>
